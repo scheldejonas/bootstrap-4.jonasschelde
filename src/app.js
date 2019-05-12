ed62daca162826faa8fb50585f8fab52275ec498
Jonas Schelde <kontakt@jonasschelde.dk>
2019-05-12 12:22:29 +0200

    v1.1.1
    Update: package got added anothe rpacke

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
 import 'bootstrap';
 
 import './scss/app.scss';
+
+import inline from 'inline-critical' ;
+
+var html = fs.readFileSync('https://unisecure.dk/', 'utf8');
+
+var critical = fs.readFileSync('https://unisecure.dk/wp-content/cache/min/1/wp-content/themes/pro/framework/dist/css/site/stacks/renew-a8671f2383c22dd3e3be71baf498c988.css', 'utf8');
+
+var inlined = inline(html, critical);
+
+console.log( inlined );
